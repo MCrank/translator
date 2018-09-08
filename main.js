780ec0a6a609ee9187e1e3e1209b5af5adf8033a
MCrank <marco_mcse@hotmail.com>
2018-09-08 10:48:10 -0500

    Added the translation objects for three languages

---
+++ main.js
@@ -0,0 +1,26 @@
+let greekTranslationObj = {
+  merry: 'Καλά',
+  christmas: 'Χριστούγεννα',
+  and: 'Και',
+  happy: 'Ευτυχισμένο',
+  new: 'Nwe',
+  year: 'Έτος'
+};
+
+let germanTranslationObj = {
+  merry: 'Frohe',
+  christmas: 'Weihnachten',
+  and: 'Und',
+  happy: 'Glücklich',
+  new: 'Neu',
+  year: 'Jahr'
+};
+
+let russianTranslationObj = {
+  merry: 'Веселого',
+  christmas: 'Рождество',
+  and: 'И',
+  happy: 'Счастлив',
+  new: 'Новые функции',
+  year: 'Год'
+};
